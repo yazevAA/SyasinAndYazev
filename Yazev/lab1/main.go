5076f5ff1c8385e9026a1503ca85e84b27fe8b29
yazevAA <legionondrey@gmail.com>
2026-09-04 22:37:59 +0300

    Поставил более смешные цифры

--- Yazev/lab1/main.go
+++ Yazev/lab1/main.go
@@ -1,15 +1,17 @@
 package main
 
-import "fmt"
-
-var (
-	id       string = "ID"
-	name     string = "Имя"
-	sallary  string = "З/П"
+import (
+	"fmt"
 )
 
-var idlist = []int{67, 69, 52, 911, 404, 812, 415, 417}
-var namelist = []string{
+var (
+	id      string = "ID"
+	name    string = "Имя"
+	sallary string = "З/П"
+)
+
+var idList = []int{67, 69, 52, 911, 1488, 812, 1312, 417}
+var nameList = []string{
 	"Пупкин Алеша",
 	"Кукушкин Таракан",
 	"Дураков Фотий",
@@ -17,9 +19,9 @@ var namelist = []string{
 	"Криворуков Аполлон",
 	"Сясин Сергей",
 	"Терушкин Борис",
-	"Шавалиев Альберт",
-}
-var sallarylist = []float32{23570.91, 6767.6767, 2004.1512, 150000, 409.1308, 25000, 1634234, 0.01}
+	"Шавалиев Альберт"}
+
+var sallaryList = []float32{23570.91, 6767.6767, 2004.1512, 150000, 409.1308, 25000, 1634234, 0.01}
 
 func main() {
 
@@ -27,8 +29,8 @@ func main() {
 	fmt.Printf("|%4s | %-30s | %-10s |\n", id, name, sallary)
 	fmt.Printf("-----------------------------------------------------\n")
 
-	for i := 0; i < len(idlist); i++ {
-		fmt.Printf("|%4d | %-30s | %10.2f |\n", idlist[i], namelist[i], sallarylist[i])
+	for i := 0; i < len(idList); i++ {
+		fmt.Printf("|%4d | %-30s | %10.2f |\n", idList[i], nameList[i], sallaryList[i])
 	}
 
 	fmt.Printf("-----------------------------------------------------\n")
