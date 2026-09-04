bb515f77381104171a6ebc8e763c0b4bad09c702
yazevAA <legionondrey@gmail.com>
2026-09-04 17:43:29 +0300

    123

--- Yazev/main.go
+++ Yazev/main.go
@@ -5,7 +5,7 @@ import "fmt"
 var (
 	id       string = "ID"
 	name     string = "Имя"
-	sallary  string = "З П"
+	sallary  string = "З/П"
 )
 
 var idlist = []int{67, 69, 52, 911, 404, 812, 415, 417}
@@ -23,16 +23,14 @@ var sallarylist = []float32{23570.91, 6767.6767, 2004.1512, 150000, 409.1308, 25
 
 func main() {
 
-	fmt.Printf("|-----|--------------------------------|------------|\n")
+	fmt.Printf("-----------------------------------------------------\n")
 	fmt.Printf("|%4s | %-30s | %-10s |\n", id, name, sallary)
-	fmt.Printf("|-----|--------------------------------|------------|\n")
+	fmt.Printf("-----------------------------------------------------\n")
 
 	for i := 0; i < len(idlist); i++ {
 		fmt.Printf("|%4d | %-30s | %10.2f |\n", idlist[i], namelist[i], sallarylist[i])
 	}
 
-	fmt.Printf("|-----|--------------------------------|------------|\n")
+	fmt.Printf("-----------------------------------------------------\n")
 
-	fmt.Println("\nНажмите Enter для выхода...")
-	fmt.Scanln()
 }
